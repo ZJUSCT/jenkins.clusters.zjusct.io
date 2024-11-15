e09771e1425dd61f1e031a8ccc8e9a23c2e024c4
bowling233 <zhubaolin228@gmail.com>
2024-11-15 18:10:57 +0800

    fix(fstab): ocean mount point

--- debian/modules/99-fstab.sh
+++ debian/modules/99-fstab.sh
@@ -4,6 +4,7 @@ cat >/etc/fstab <<EOF
 #172.25.2.11:/lake /lake nfs defaults 0 0
 172.25.2.11:/river /river nfs defaults 0 0
 172.25.2.11:/slurm /slurm nfs defaults 0 0
+172.25.2.11:/ocean /ocean nfs defaults 0 0
 none /tmp tmpfs defaults 0 0
 none /var/tmp tmpfs defaults 0 0
 none /var/log tmpfs defaults 0 0
