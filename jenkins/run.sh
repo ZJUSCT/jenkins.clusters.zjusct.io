6575eed3f6aadd247e0bd04343d8cc991249063b
bowling233 <zhubaolin228@gmail.com>
2024-11-15 21:29:51 +0800

    feat(jenkins): persistent Jenkins database

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -4,5 +4,5 @@ set -xe
 docker compose up -d
 
 jenkins-jobs --conf jenkins_jobs.ini test jobs.yaml >/dev/null
-sleep 10 # wait for jenkins to start
+sleep 20 # wait for jenkins to start
 jenkins-jobs --conf jenkins_jobs.ini update jobs.yaml
