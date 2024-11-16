284a838eaf8f4eaca26d008015d84f579c7c1c62
bowling233 <zhubaolin228@gmail.com>
2024-11-17 02:18:13 +0800

    feat(modules-header.sh): detect init program fix(timezone): timedatectl can't be used in docker

--- debian/modules-header.sh
+++ debian/modules-header.sh
@@ -82,3 +82,5 @@ esac
 
 ARCH=$(dpkg --print-architecture)
 KERNEL_VERSION=$(for file in /boot/vmlinuz-*; do echo "${file#/boot/vmlinuz-}"; done | sort -V | tail -n 1)
+# https://superuser.com/questions/1017959/how-to-know-if-i-am-using-systemd-on-linux
+INIT=$(ps --no-headers -o comm 1)
--- debian/modules/98-timezone.sh
+++ debian/modules/98-timezone.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 # Set TimeZone To Asia/Shanghai
 
-timedatectl set-timezone Asia/Shanghai
+case $INIT in
+  systemd)
+    timedatectl set-timezone Asia/Shanghai
+    ;;
+  *)
+    rm -rf /etc/localtime
+    ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+    ;;
+esac
