08ed752513de78463f77506a3a93efe2cb5dd202
朱宝林 <zhubaolin228@gmail.com>
2024-12-07 13:32:07 +0000

    feat(debian): add attr

--- debian/modules/01-apt-basic.sh
+++ debian/modules/01-apt-basic.sh
@@ -44,7 +44,7 @@ install_special_pkgs() {
 
 PACKAGES_COMMON=(
 	aptitude build-essential clang clang-format clang-tidy cmake clangd ccls ethtool
-	cpu-checker htop iperf3 net-tools rename
+	cpu-checker htop iperf3 net-tools rename attr
 	cpufetch curl devscripts doxygen exfatprogs fish fonts-firacode gcc-doc
 	gfortran git-extras git-lfs glibc-doc hwinfo hwloc ipmitool iptraf-ng jq
 	libgmp-dev libgtk-3-dev libpcap-dev linux-source lldb lldpd locate mc meson
