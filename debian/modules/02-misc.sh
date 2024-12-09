ba2454e9517d0c9a6d0ed9bb5a2fe86e9199e542
bowling233 <zhubaolin228@gmail.com>
2024-12-09 15:49:55 +0800

    feat(02-misc.sh): add gdg

--- debian/modules/02-misc.sh
+++ debian/modules/02-misc.sh
@@ -2,6 +2,10 @@
 
 apt install zip
 
+# bitwarden/clients
 tmpfile=$(mktemp -u).zip
 get_asset_from_github 'bitwarden/clients' 'startswith("bw-linux") and endswith(".zip")' "$tmpfile"
 unzip -d /usr/local/bin "$tmpfile"
+
+# esnet/gdg
+install_deb_from_github 'esnet/gdg' 'endswith("amd64.deb")'
