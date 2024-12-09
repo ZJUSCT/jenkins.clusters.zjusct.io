73012c7fa8cb2914e51434b532ebf040e1e18c5e
朱宝林 <zhubaolin228@gmail.com>
2024-12-09 11:26:56 +0000

    Update 02-misc.sh

--- debian/modules/02-misc.sh
+++ debian/modules/02-misc.sh
@@ -9,3 +9,6 @@ unzip -d /usr/local/bin "$tmpfile"
 
 # esnet/gdg
 install_deb_from_github 'esnet/gdg' 'endswith("amd64.deb")'
+
+# wagoodman/dive
+install_deb_from_github 'wagoodman/dive' 'endswith("amd64.deb")'
