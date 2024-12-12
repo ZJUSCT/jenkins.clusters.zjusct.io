94645fd76fc40c6aeba847428a38758c89758f52
bowling233 <zhubaolin228@gmail.com>
2024-12-12 10:57:32 +0800

    fix(arch): missing pkg

--- modules/02-software-misc.sh
+++ modules/02-software-misc.sh
@@ -2,6 +2,12 @@
 
 # bitwarden/clients
 install_pkg zip
+case $ID in
+arch)
+	install_pkg unzip
+	;;
+esac
+
 tmpfile=$(mktemp -u).zip
 get_asset_from_github 'bitwarden/clients' 'startswith("bw-linux") and endswith(".zip")' "$tmpfile"
 unzip -d /usr/local/bin "$tmpfile"
