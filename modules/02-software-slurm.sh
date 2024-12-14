c696c2ad38ebbf5bd4b511480eb8e50635e2316e
bowling233 <zhubaolin228@gmail.com>
2024-12-15 01:56:09 +0800

    fix(arch): add slurm support

--- modules/02-software-slurm.sh
+++ modules/02-software-slurm.sh
@@ -8,4 +8,42 @@ ubuntu(){
 	debian
 }
 
+arch(){
+	install_pkg munge slurm-llnl
+}
+
+openEuler(){
+	install_pkg munge slurm
+}
+
+configure(){
+	# Munge systemd service override
+	mkdir /etc/systemd/system/munge.service.d
+	cat >/etc/systemd/system/munge.service.d/override.conf <<EOF
+[Unit]
+After=remote-fs.target
+ConditionPathExists=/slurm/etc/default/munge
+[Service]
+EnvironmentFile=
+EnvironmentFile=-/slurm/etc/default/munge
+EOF
+
+	# Slurm systemd service override
+	mkdir /etc/systemd/system/slurmd.service.d
+	cat >/etc/systemd/system/slurmd.service.d/override.conf <<EOF
+[Unit]
+After=remote-fs.target
+ConditionPathExists=/slurm/etc/default/slurmd
+[Service]
+EnvironmentFile=
+EnvironmentFile=-/slurm/etc/default/slurmd
+EOF
+
+	# Slurm utils need SLURM_CONF env
+	cat >>/etc/environment <<EOF
+SLURM_CONF="/slurm/etc/slurm/slurm.conf"
+EOF
+}
+
 check_and_exec "$ID"
+configure
