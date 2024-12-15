c70a797a596ba454d69e15efbaa2709550c05288
bowling233 <zhubaolin228@gmail.com>
2024-12-15 20:40:59 +0800

    fix: pkgs for debian testing and openEuler

--- modules/02-driver-rdma.sh
+++ modules/02-driver-rdma.sh
@@ -2,9 +2,12 @@
 
 debian() {
 	# https://wiki.debian.org/RDMA
-	install_pkg rdma-core ibutils mstflint perftest libibverbs1 librdmacm1 \
+	install_pkg rdma-core mstflint perftest libibverbs1 librdmacm1 \
 		libibmad5 libibumad3 librdmacm1 ibverbs-providers rdmacm-utils \
 		infiniband-diags libfabric1 ibverbs-utils
+	if ! [[ $RELEASE == "testing" ]]; then
+		install_pkg ibutils
+	fi
 }
 
 ubuntu() {
