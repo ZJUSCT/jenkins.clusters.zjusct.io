d75548949c5197b7a76d673bbb7ef70dda33209c
bowling233 <zhubaolin228@gmail.com>
2024-12-15 21:35:07 +0800

    fix(openEuler): pkgs

--- modules/02-software-container.sh
+++ modules/02-software-container.sh
@@ -14,7 +14,6 @@ else
 fi
 
 debian_docker() {
-
 	# https://docs.docker.com/engine/install/ubuntu/
 	# https://docs.docker.com/engine/install/debian/
 	# https://docs.docker.com/engine/install/linux-postinstall/
@@ -34,15 +33,20 @@ deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] $
 EOF
 	apt-get update
 
-	apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+	install_pkg docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 }
 
 ubuntu_docker() {
 	debian_docker
 }
+
 arch_docker() {
 	# https://wiki.archlinux.org/title/Docker
-	pacman -S docker docker-buildx docker-compose containerd
+	install_pkg docker docker-buildx docker-compose containerd
+}
+
+openEuler() {
+	install_pkg docker-
 }
 
 check_and_exec "$ID"_docker
@@ -53,11 +57,17 @@ check_and_exec "$ID"_docker
 debian_apptainer() {
 	install_pkg_from_github apptainer/apptainer 'contains("apptainer")'
 }
+
 ubuntu_apptainer() {
 	debian_apptainer
 }
+
 arch_apptainer() {
-	pacman -S apptainer
+	install_pkg apptainer
+}
+
+openEuler_apptainer() {
+	echo "not planned"
 }
 
 check_and_exec "$ID"_apptainer
