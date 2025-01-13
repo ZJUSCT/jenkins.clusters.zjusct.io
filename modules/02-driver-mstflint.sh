5efb4e7009f203065097ec4c4e64d791a5ed67ea
bowling233 <zhubaolin228@gmail.com>
2025-01-13 10:54:32 +0800

    fix(mstflint): use distro package

--- modules/02-driver-mstflint.sh
+++ modules/02-driver-mstflint.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 # https://github.com/Mellanox/mstflint
 
+# Debian packaged mstflint
+
 case $ID in
 debian|ubuntu)
-	install_pkg libibmad-dev
+	install_pkg libibmad-dev mstflint
+	;;
+openEuler)
+	install_pkg mstflint
+	;;
+*)
+	get_asset_from_github "Mellanox/mstflint" 'startswith("mstflint")' /tmp/mstflint.tar.gz
+	tar xf /tmp/mstflint.tar.gz -C /tmp
+	rm -f /tmp/mstflint.tar.gz
+	cd /tmp/mstflint* || exit
+	./configure
+	make -j"$(nproc)"
+	make install
 	;;
 esac
-
-get_asset_from_github "Mellanox/mstflint" 'startswith("mstflint")' /tmp/mstflint.tar.gz
-tar xf /tmp/mstflint.tar.gz -C /tmp
-cd /tmp/mstflint* || exit
-./configure
-make -j"$(nproc)"
-make install
