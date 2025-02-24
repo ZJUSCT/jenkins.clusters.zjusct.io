381c14efe7076c2c3adcb6c9f300a25e450d0d00
mrhaoxx <mr.haoxx@gmail.com>
2025-02-24 22:11:25 +0800

    update spack function

--- modules/02-pkgmgr-spack.sh
+++ modules/02-pkgmgr-spack.sh
@@ -2,19 +2,22 @@
 
 cat >/etc/profile.d/z00_spack.sh <<EOF
 spack() {
-    export https_proxy="$CACHE_PROXY_EX"
-    export http_proxy="$CACHE_PROXY_EX"
-    command spack "\$@"
+    export https_proxy="http://storage:3128"
+    export http_proxy="http://storage:3128"
+    if ! type -P spack >/dev/null 2>&1; then
+        . ~/spack/share/spack/setup-env.sh
+        . /etc/profile.d/z00_spack.sh 
+    fi
+    command spack "$@"
 }
 EOF
 
 # fish
 cat >/etc/fish/conf.d/z00_spack.fish <<EOF
 spack() {
-    export https_proxy="$CACHE_PROXY_EX"
-    export http_proxy="$CACHE_PROXY_EX"
+    export https_proxy="http://storage:3128"
+    export http_proxy="http://storage:3128"
     command spack "\$@"
 }
 EOF
-
 
