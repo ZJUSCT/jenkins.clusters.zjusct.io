e05600b3d4471bd0acf834975f540aef7eed7c98
mrhaoxx <mr.haoxx@gmail.com>
2025-02-24 22:47:34 +0800

    fix spack

--- modules/02-pkgmgr-spack.sh
+++ modules/02-pkgmgr-spack.sh
@@ -6,10 +6,10 @@ spack() {
     export http_proxy="http://storage:3128"
     if ! type -P spack >/dev/null 2>&1; then
         local _spack_def
-        _spack_def=$(declare -f spack)
+        _spack_def=\$(declare -f spack)
         unset -f spack
         . ~/spack/share/spack/setup-env.sh
-        eval "$_spack_def"
+        eval "\$_spack_def"
     fi
     command spack \$@
 }
