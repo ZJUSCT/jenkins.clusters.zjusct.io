50f1d8558b895dbd96e89b28c095adde9c9419cf
mrhaoxx <mr.haoxx@gmail.com>
2025-02-24 21:25:43 +0800

    fix ctld deb

--- modules/03-software-ctld.sh
+++ modules/03-software-ctld.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl https://gitlab.star-home.top:4430/star/deploy-ctld/-/blob/745728db34cdf940fa547b7831f9140bd6ca9757/ctld_1.0.0_amd64.deb -o /tmp/ctld_1.0.0_amd64.deb
+curl https://gitlab.star-home.top:4430/star/deploy-ctld/-/raw/main/ctld_1.0.0_amd64.deb -o /tmp/ctld_1.0.0_amd64.deb
 
 dpkg -i /tmp/ctld_1.0.0_amd64.deb
 
