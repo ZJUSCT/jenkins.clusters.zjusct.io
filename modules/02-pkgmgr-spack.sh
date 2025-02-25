9c246d1cf9dc0a2c4ca3386de6ad8441ac2a5b1d
mrhaoxx <mr.haoxx@gmail.com>
2025-02-25 16:46:00 +0800

    update spack function

--- modules/02-pkgmgr-spack.sh
+++ modules/02-pkgmgr-spack.sh
@@ -2,16 +2,23 @@
 
 cat >/etc/profile.d/z00_spack.sh <<EOF
 spack() {
-    export https_proxy="http://storage:3128"
-    export http_proxy="http://storage:3128"
-    if ! type -P spack >/dev/null 2>&1; then
+    export old_https_proxy=\$https_proxy
+    export old_http_proxy=\$http_proxy
+    export https_proxy="http://storage:8083"
+    export http_proxy="http://storage:8083"
+    if ! type _spack_shell_wrapper >/dev/null 2>&1; then
         local _spack_def
         _spack_def=\$(declare -f spack)
         unset -f spack
         . ~/spack/share/spack/setup-env.sh
         eval "\$_spack_def"
     fi
-    command spack \$@
+   _spack_shell_wrapper "\$@"
+    export https_proxy=\$old_https_proxy
+    export http_proxy=\$old_http_proxy
+    unset old_https_proxy
+    unset old_http_proxy
+    return \$?
 }
 EOF
 
