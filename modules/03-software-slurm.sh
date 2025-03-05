766aee6b730cb73a881b57a36481a38e56cd413e
mrhaoxx <mr.haoxx@gmail.com>
2025-03-05 21:10:06 +0800

    update slurm

--- modules/03-software-slurm.sh
+++ modules/03-software-slurm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # https://github.com/dun/munge/issues/92
-groupadd --gid 701 munge
-useradd --comment "MUNGE authentication service" \
-	--uid 701 \
-	--gid munge \
-	--shell /sbin/nologin \
-	--home-dir /usr/local/etc/munge \
-	munge
+# groupadd --gid 701 munge
+# useradd --comment "MUNGE authentication service" \
+# 	--uid 701 \
+# 	--gid munge \
+# 	--shell /sbin/nologin \
+# 	--home-dir /usr/local/etc/munge \
+# 	munge
 
 # see Debian source
 # slurm-wlm/debian/slurm-wlm-basic-plugins.preinst/
@@ -38,26 +38,54 @@ openEuler() {
 
 check_and_exec "$ID"
 
-# Munge systemd service override
-mkdir /etc/systemd/system/munge.service.d
-cat >/etc/systemd/system/munge.service.d/override.conf <<EOF
+cat > /lib/systemd/system/munge.service  <<EOF
 [Unit]
-After=remote-fs.target
-ConditionPathExists=/slurm/etc/default/munge
+Description=MUNGE authentication service
+Documentation=man:munged(8)
+Wants=sssd-nss.service
+After=network-online.target
+After=time-sync.target
+After=sockets.target
+After=sssd-nss.service
+
 [Service]
-EnvironmentFile=
+Type=forking
 EnvironmentFile=-/slurm/etc/default/munge
+ExecStart=/usr/sbin/munged $OPTIONS
+PIDFile=/run/munge/munged.pid
+RuntimeDirectory=munge
+RuntimeDirectoryMode=0755
+User=munge
+Group=munge
+Restart=on-abort
+
+[Install]
+WantedBy=multi-user.target
 EOF
 
-# Slurm systemd service override
-mkdir /etc/systemd/system/slurmd.service.d
-cat >/etc/systemd/system/slurmd.service.d/override.conf <<EOF
+cat >/etc/systemd/system/slurmd.service <<EOF
 [Unit]
-After=remote-fs.target
+Description=Slurm node daemon
+After=munge.service network-online.target remote-fs.target
+Wants=network-online.target
 ConditionPathExists=/slurm/etc/default/slurmd
+Documentation=man:slurmd(8)
+
 [Service]
-EnvironmentFile=
+Type=simple
 EnvironmentFile=-/slurm/etc/default/slurmd
+ExecStart=/usr/sbin/slurmd -D -s $SLURMD_OPTIONS
+ExecReload=/bin/kill -HUP $MAINPID
+PIDFile=/run/slurmd.pid
+KillMode=process
+LimitNOFILE=131072
+LimitMEMLOCK=infinity
+LimitSTACK=infinity
+Delegate=yes
+TasksMax=infinity
+
+[Install]
+WantedBy=multi-user.target
 EOF
 
 # Slurm utils need SLURM_CONF env
