b0342d74f2338758bef10bba18024cdfa4d70d70
mrhaoxx <mr.haoxx@gmail.com>
2025-03-07 15:17:06 +0800

    update ctld

--- modules/03-software-ctld.sh
+++ modules/03-software-ctld.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl https://gitlab.star-home.top:4430/star/deploy-ctld/-/raw/main/ctld_1.0.0_amd64.deb -o /tmp/ctld_1.0.0_amd64.deb
+curl https://gitlab.star-home.top:4430/star/deploy-ctld/-/raw/main/ctld_1.0.1_amd64.deb -o /tmp/ctld_1.0.1_amd64.deb
 
-dpkg -i /tmp/ctld_1.0.0_amd64.deb
+dpkg -i /tmp/ctld_1.0.1_amd64.deb
 
 systemctl enable ctld
