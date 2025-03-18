77fecc56f9689f9b3a2a613e36ed77940157d46f
mrhaoxx <mr.haoxx@gmail.com>
2025-03-18 20:54:50 +0800

    update

--- modules/03-software-ctld.sh
+++ modules/03-software-ctld.sh
@@ -4,3 +4,19 @@ curl https://gitlab.star-home.top:4430/star/deploy-ctld/-/raw/main/ctld_1.0.1_am
 
 dpkg -i /tmp/ctld_1.0.1_amd64.deb
 
+cat << EOF > /etc/systemd/system/ctld.service
+[Unit]
+Description=Control Daemon
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/ctld client -server 172.25.4.11:4320
+Restart=always
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
