f2da2202270f434dd699cb632bf2a1ce753fb372
bowling233 <zhubaolin228@gmail.com>
2025-03-24 23:47:06 +0800

    fix: dracut upstream branch master removed 404

--- modules/98-initramfs.sh
+++ modules/98-initramfs.sh
@@ -15,11 +15,12 @@ esac
 
 # from debian source
 # https://salsa.debian.org/debian/dracut/-/tree/master/debian/90overlay-root?ref_type=heads
+# branch varies, so choose a specific commit
 if [ ! -d /usr/lib/dracut/modules.d/90overlay-root ]; then
 	mkdir /usr/lib/dracut/modules.d/90overlay-root
-	wget -O /usr/lib/dracut/modules.d/90overlay-root/module-setup.sh https://salsa.debian.org/debian/dracut/-/raw/master/debian/90overlay-root/module-setup.sh
-	wget -O /usr/lib/dracut/modules.d/90overlay-root/overlay-mount.sh https://salsa.debian.org/debian/dracut/-/raw/master/debian/90overlay-root/overlay-mount.sh
-	wget -O /usr/lib/dracut/modules.d/90overlay-root/README https://salsa.debian.org/debian/dracut/-/raw/master/debian/90overlay-root/README
+	wget -O /usr/lib/dracut/modules.d/90overlay-root/module-setup.sh https://salsa.debian.org/debian/dracut/-/raw/939c2b9673f0ccb77aca32e3b602b2c1c2b52d86/debian/90overlay-root/module-setup.sh
+	wget -O /usr/lib/dracut/modules.d/90overlay-root/overlay-mount.sh https://salsa.debian.org/debian/dracut/-/raw/939c2b9673f0ccb77aca32e3b602b2c1c2b52d86/debian/90overlay-root/overlay-mount.sh
+	wget -O /usr/lib/dracut/modules.d/90overlay-root/README https://salsa.debian.org/debian/dracut/-/raw/939c2b9673f0ccb77aca32e3b602b2c1c2b52d86/debian/90overlay-root/README
 fi
 
 # options nvidia NVreg_RestrictProfilingToAdminUsers=0 for nvidia driver
