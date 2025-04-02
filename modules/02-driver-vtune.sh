a6f298bbb307bc3cb7b515ccc8a4a60dede3a837
bowling233 <zhubaolin228@gmail.com>
2025-04-02 12:14:24 +0800

    fix(driver)

--- modules/02-driver-vtune.sh
+++ modules/02-driver-vtune.sh
@@ -3,6 +3,10 @@
 # https://www.intel.com/content/www/us/en/developer/articles/code-sample/vtune-profiler-sampling-driver-downloads.html
 # https://www.intel.com/content/www/us/en/docs/vtune-profiler/user-guide/2024-2/build-install-sampling-drivers-for-linux-targets.html
 
+set -x
+
+export
+
 URL="https://cdrdv2.intel.com/v1/dl/getContent/778889"
 
 cd /tmp || exit 1
@@ -12,7 +16,7 @@ tar -xzf vtune.tar.gz -C /opt/intel/vtune/sepdk --strip-components=1
 cd /opt/intel/vtune/sepdk/src || exit 1
 ./build-driver --non-interactive \
 	--kernel-version="$KERNEL_VERSION" \
-	--kernel-src-dir="/usr/src/linux-headers-${KERNEL_VERSION//-amd64/}-common" \
+	--kernel-src-dir="/usr/src/linux-headers-${KERNEL_VERSION}-common/include" \
 # ./rmmod-sep -s
 ./insmod-sep --no-udev
 ./boot-script --install
