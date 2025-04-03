16e57a4b3917b1b6278c66172cd9c368693cd3bd
bowling233 <zhubaolin228@gmail.com>
2025-04-03 12:56:27 +0800

    fix(ctld): curl 403, try wget

--- modules/03-software-ctld.sh
+++ modules/03-software-ctld.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 debian() {
-    curl https://gitlab.star-home.top:4430/star/deploy-ctld/-/raw/main/ctld_1.0.1_amd64.deb -o /tmp/ctld_1.0.1_amd64.deb
+    wget -O /tmp/ctld_1.0.1_amd64.deb https://gitlab.star-home.top:4430/star/deploy-ctld/-/raw/main/ctld_1.0.1_amd64.deb
     dpkg -i /tmp/ctld_1.0.1_amd64.deb
-
 }
 
 ubuntu() {
